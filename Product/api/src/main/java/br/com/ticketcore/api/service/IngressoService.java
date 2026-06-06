eb07ec5c7bf977427dfc1be6791ab329cd3d64bc
lucasborgss1 <lucasaborgesdev1@gmail.com>
2026-06-06 17:50:24 -0300

    service: atualizados serviços com novas regras de negócio e validações

--- Product/api/src/main/java/br/com/ticketcore/api/service/IngressoService.java
+++ Product/api/src/main/java/br/com/ticketcore/api/service/IngressoService.java
@@ -1,6 +1,7 @@
 package br.com.ticketcore.api.service;
 
 import br.com.ticketcore.api.dao.IngressoDAO;
+import br.com.ticketcore.api.exception.RecursoNaoEncontradoException;
 import br.com.ticketcore.api.model.Ingresso;
 import br.com.ticketcore.api.model.enums.StatusIngresso;
 import org.springframework.stereotype.Service;
@@ -16,20 +17,25 @@ public class IngressoService {
         this.ingressoDAO = ingressoDAO;
     }
 
-    public Ingresso buscarPorId(Long id) {
-        return ingressoDAO.buscarPorId(id);
+    public Ingresso buscarPorId(Long id, Long idComprador) {
+        Ingresso ingresso = ingressoDAO.buscarPorIdEComprador(id, idComprador);
+        if (ingresso == null) throw new RecursoNaoEncontradoException();
+        return ingresso;
     }
 
-    public Ingresso buscarPorCodigoAcesso(String codigoAcesso) {
-        return ingressoDAO.buscarPorCodigoAcesso(codigoAcesso);
+    public Ingresso buscarPorCodigoAcesso(String codigoAcesso, Long idComprador) {
+        Ingresso ingresso = ingressoDAO.buscarPorCodigoEComprador(codigoAcesso, idComprador);
+        if (ingresso == null) throw new RecursoNaoEncontradoException();
+        return ingresso;
     }
 
-    public List<Ingresso> listarPorTransacao(Long idTransacao) {
-        return ingressoDAO.listarPorTransacao(idTransacao);
+    public List<Ingresso> listarPorTransacao(Long idTransacao, Long idComprador) {
+        return ingressoDAO.listarPorTransacaoEComprador(idTransacao, idComprador);
     }
 
-    public void cancelar(Long id) {
-        Ingresso ingresso = ingressoDAO.buscarPorId(id);
+    public void cancelar(Long id, Long idComprador) {
+        Ingresso ingresso = ingressoDAO.buscarPorIdEComprador(id, idComprador);
+        if (ingresso == null) throw new RecursoNaoEncontradoException();
         ingresso.setStIngresso(StatusIngresso.CANCELADO);
         ingressoDAO.atualizar(ingresso);
     }
